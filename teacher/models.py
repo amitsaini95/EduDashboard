34018e149bf780056e4b6d1765e192abc5e974fe
Amit Saini <amitsaini95>
2024-05-18 13:04:33 +0530

    teacher verify by school

--- teacher/models.py
+++ teacher/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from student.models import *
 from school.models import *
 
-
+VERIFY=(
+    ('Verify','Verify'),
+    ('NotVerify','NotVerify')
+)
 # Create your models here.
 class TeacherProfileModel(models.Model):
     name=models.CharField(max_length=80)
@@ -13,12 +16,14 @@ class TeacherProfileModel(models.Model):
     profile=models.ImageField(upload_to="teacherProfile",null=True,blank=True)
     address=models.TextField()
     salary=models.IntegerField(blank=True,null=True)
-    teacherVerifyBySchool=models.BooleanField(max_length=20,default=False)
+    teacherVerifyBySchool=models.CharField(max_length=20,default='NotVerify',choices=VERIFY)
     city=models.ForeignKey('student.city',on_delete=models.CASCADE,related_name="teacherCity")
     state=models.ForeignKey('student.state',on_delete=models.CASCADE,related_name="teacherState")
     role=models.CharField(max_length=100)
     teachOfClass=models.CharField(max_length=20)
     totalOfStudent=models.ManyToManyField('student.StudentProfileModel',null=True,blank=True)
+
+
     def __str__(self):
         return self.name
 
